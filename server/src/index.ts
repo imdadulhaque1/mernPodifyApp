d827e9fdf1dde9241ce9de467d45135ac77a427d
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-09-02 12:20:04 +0600

    Server environment initially Setup Done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import "dotenv/config"; //!dotenv must be before than db
+import "./db";
 
 const app = express();
-const PORT = 8989;
+const PORT = process.env.PORT || 8989;
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
