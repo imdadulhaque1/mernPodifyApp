a295dea5fd64b688f52a47680932f18198bfd886
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-09-29 19:26:38 +0600

    Sending real email to mailTrap

--- server/src/api/controllers/userController.ts
+++ server/src/api/controllers/userController.ts
@@ -5,6 +5,8 @@ import { CreateUser } from "@/@types/userTypes";
 import { CreateUserSchema } from "@/utils/validationSchema";
 import UserModel from "#/models/UserModel";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "@/utils/variables";
+import { generateToken } from "@/utils/helper";
+import EmailVerificationTokenModel from "../models/EmailVerificationTokenModel";
 
 export const createUser: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
@@ -22,10 +24,17 @@ export const createUser: RequestHandler = async (req: CreateUser, res) => {
       pass: MAILTRAP_PASS,
     },
   });
+
+  const token = generateToken();
+  await EmailVerificationTokenModel.create({
+    owner: user._id,
+    token,
+  });
+
   transport.sendMail({
     to: user.email,
     from: "imdadulhaque1440@gmail.com",
-    html: "<h1>Testing Email Connections</h1>",
+    html: `<h1>Your verification token is ${token}</h1>`,
   });
 
   res.status(201).json({ message: "User successfully created!", user });
