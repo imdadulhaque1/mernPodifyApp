55ec335bb76ba8a7d1380c39b7d4c9c1dad9a92b
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2023-10-04 21:43:23 +0600

    Added mail template and sending mail with attachments

--- server/src/api/controllers/userController.ts
+++ server/src/api/controllers/userController.ts
@@ -7,6 +7,7 @@ import UserModel from "#/models/UserModel";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "@/utils/variables";
 import { generateToken } from "@/utils/helper";
 import EmailVerificationTokenModel from "../models/EmailVerificationTokenModel";
+import { sendVerificationMail } from "@/utils/mail";
 
 export const createUser: RequestHandler = async (req: CreateUser, res) => {
   const { name, email, password } = req.body;
@@ -15,27 +16,13 @@ export const createUser: RequestHandler = async (req: CreateUser, res) => {
   });
   const user = await UserModel.create({ name, email, password });
 
-  // Send Verification Email
-  var transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASS,
-    },
-  });
-
   const token = generateToken();
-  await EmailVerificationTokenModel.create({
-    owner: user._id,
-    token,
-  });
+  sendVerificationMail(token, { name, email, userId: user._id.toString() });
 
-  transport.sendMail({
-    to: user.email,
-    from: "imdadulhaque1440@gmail.com",
-    html: `<h1>Your verification token is ${token}</h1>`,
-  });
-
-  res.status(201).json({ message: "User successfully created!", user });
+  res
+    .status(201)
+    .json({
+      message: "User successfully created!",
+      user: { id: user._id, name, email },
+    });
 };
--- server/src/utils/variables.ts
+++ server/src/utils/variables.ts
@@ -1,2 +1,3 @@
 const { env } = process as { env: { [key: string]: string } };
-export const { MONGO_URI, MAILTRAP_USER, MAILTRAP_PASS } = env;
+export const { MONGO_URI, MAILTRAP_USER, MAILTRAP_PASS, VERIFICATION_EMAIL } =
+  env;
